cffdb41feb725b2912c4ede36a65cc8fcd6ab268
MTSistemi <61477413+MTSistemi@users.noreply.github.com>
2020-11-21 00:36:08 +0100

    Create install_omv5.sh

---
+++ install_omv5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Autor: Mattia Tadini
+# File: install_omv5.sh
+# Revision: 1.00
+
+echo "Now We Download And Install OpenMediaVault 5..."
+echo
+echo "Please Wait This Operation Take A Longtime"
+wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
